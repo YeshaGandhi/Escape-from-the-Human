b22b0cf587d2ff5fbaf3e3e11b297dff60e6c1e5
YeshaGandhi <69881405+YeshaGandhi@users.noreply.github.com>
2021-01-17 22:13:43 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Escape-from-the-Human
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,30 @@
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  
+  var wall1=createSprite(250,150,5,100)
+  var wall2=createSprite(330,150,5,100)
+  var wall3=createSprite(700,150,5,100)
+  var wall4=createSprite(780,150,5,100)
+  var wall5=createSprite(153,200,200,5)
+  var wall6=createSprite(153,280,200,5)
+  var wall7=createSprite(250,330,5,100)
+  var wall8=createSprite(330,330,5,100)
+  var wall9=createSprite(415,200,177,5)
+  var wall10=createSprite(377,280,100,5)
+  var wall11=createSprite(430,425,5,300)
+  var wall12=createSprite(500,330,5,255)
+  var wall13=createSprite(520,577,180,5)
+  var wall14=createSprite(598,460,200,5)
+
+  
+
+
+  
+
+
+}
+
+function draw() {
+  background(0);  
+  drawSprites();
+}
